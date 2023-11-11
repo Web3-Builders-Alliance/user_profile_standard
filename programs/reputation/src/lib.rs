5c41ce66acf7ae8d85930294cf6d180b3280caf2
victor_tk <victorkdevelopment@gmail.com>
2023-11-11 20:49:37 +0200

    Update Source add and subtract pointsOC

--- programs/reputation/src/lib.rs
+++ programs/reputation/src/lib.rs
@@ -5,12 +5,12 @@ declare_id!("4GTMqydNGdDr7kKKHsZU7gJkq261HpmjTZohd5oPoThK");
 #[program]
 pub mod reputation {
     use super::*;
-    pub fn create_reputation_account(ctx: Context<Initialize>, password : String) -> Result<()> {
+    pub fn create_reputation_account(ctx: Context<Initialize>, ) -> Result<()> {
         ctx.accounts.reputation.sources_count = 0 ;
         ctx.accounts.reputation.attached_account = ctx.accounts.payer.key();
         Ok(())
     }
-    pub fn create_source_account(ctx: Context<CreateSource>, password: String, source_name: String ) -> Result<()> {
+    pub fn create_source_account(ctx: Context<CreateSource>, source_name: String ) -> Result<()> {
         ctx.accounts.source.points = 0 ;
         ctx.accounts.source.name = source_name ;
         ctx.accounts.reputation.sources_count = ctx.accounts.reputation.sources_count + 1 ;
@@ -18,26 +18,45 @@ pub mod reputation {
         msg!("SOurce created");    
         Ok(())
     }
+    pub fn add_reputation (ctx: Context<UpdateSource>,source_name: String, bonus: u64 ) -> Result<()> {
+        ctx.accounts.source.points = ctx.accounts.source.points + bonus ;
+        Ok(())
+    }
+    pub fn subtract_reputation(ctx: Context<UpdateSource>,source_name: String, penalty: u64 ) -> Result<()> {
+        ctx.accounts.source.points = ctx.accounts.source.points - penalty ;
+        Ok(())
+    } 
 }
 
 #[derive(Accounts)]
-#[instruction(password: String, source_name: String )]
+#[instruction(source_name: String )]
+pub struct UpdateSource<'info> {
+    #[account(mut)]
+    payer: Signer<'info> ,
+    #[account(mut, seeds=[b"reputation", payer.key().as_ref()], bump,)]
+    reputation: Account<'info , Reputation> ,    
+    #[account(mut,seeds=[b"source", source_name.as_bytes(), reputation.key().as_ref()],bump)]
+    source: Account<'info , Source> ,    
+}
+
+#[derive(Accounts)]
+#[instruction(source_name: String )]
 pub struct CreateSource<'info> {
     #[account(mut)]
     payer: Signer<'info> ,
-    #[account(mut, seeds=[b"reputation", password.as_bytes().as_ref()], bump,)]
+    #[account(mut, seeds=[b"reputation", payer.key().as_ref()], bump,)]
     reputation: Account<'info , Reputation> ,    
-    #[account(init, payer=payer, seeds=[ source_name.as_bytes(), reputation.key().as_ref()], bump, space= 8 + Source::INIT_SPACE)]
+    #[account(init, payer=payer, seeds=[b"source", source_name.as_bytes(), reputation.key().as_ref()], bump, space= 8 + Source::INIT_SPACE)]
     source: Account<'info , Source> ,    
     system_program: Program<'info, System>
 }
 
 #[derive(Accounts)]
-#[instruction(password: String)]
+#[instruction()]
 pub struct Initialize<'info> {
     #[account(mut)]
     payer: Signer<'info> ,
-    #[account(init, payer=payer, seeds=[b"reputation", password.as_bytes().as_ref()], bump, space= 8 + Reputation::INIT_SPACE)]
+    #[account(init, payer=payer, seeds=[b"reputation",payer.key().as_ref()], bump, space= 8 + Reputation::INIT_SPACE)]
     reputation: Account<'info , Reputation> ,    
     system_program: Program<'info, System>
 }
--- tests/reputation.ts
+++ tests/reputation.ts
@@ -10,34 +10,63 @@ const program = anchor.workspace.Reputation as Program<Reputation>;
 
 const payer = provider.wallet as anchor.Wallet;
 // const secret = "password" 
+const [reputation] = anchor.web3.PublicKey.findProgramAddressSync(
+  [Buffer.from('reputation'),payer.publicKey.toBuffer()],
+  program.programId
+);
 
-const createRepAccount = async (secret) => {
+const addPoints = async (bonus: number, source: anchor.web3.PublicKey, sourceName: string) => {
   // Create reputation account
-  const [reputation] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from('reputation'),Buffer.from(secret)],
-    program.programId
-  );
+  const tx3 = await program.methods
+    .addReputation(
+     sourceName,
+     new anchor.BN(bonus)
+    )
+    .accounts({
+      reputation,
+      source,
+      payer: payer.payer.publicKey
+    })
+    .rpc();
+  return {tx3} 
+}
+
+const subtractPoints = async (penalty: number, source: anchor.web3.PublicKey, sourceName: string) => {
+  // Create reputation account
+  const tx4 = await program.methods
+    .subtractReputation(
+      sourceName,
+      new anchor.BN(penalty)
+    )
+    .accounts({
+      reputation,
+      source,
+      payer: payer.payer.publicKey
+    })
+    .rpc();
+  return {tx4} 
+}
+const createRepAccount = async () => {
+  // Create reputation account
   const tx = await program.methods
     .createReputationAccount(
-      secret
     )
     .accounts({
       reputation,
       payer: payer.payer.publicKey
     })
     .rpc();
-  return { reputation, tx } 
+  return {tx} 
 }
 
-const createSourceAccount = async (reputation: anchor.web3.PublicKey, password: string , sourceName: string) => {
+const createSourceAccount = async (reputation: anchor.web3.PublicKey, sourceName: string) => {
   // Create source account
   const [source] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(sourceName),reputation.toBuffer()],
+    [Buffer.from('source'), Buffer.from(sourceName),reputation.toBuffer()],
     program.programId
   );
   const tx2 = await program.methods
     .createSourceAccount(
-      password,
       sourceName,
     )
     .accounts({
@@ -51,9 +80,8 @@ const createSourceAccount = async (reputation: anchor.web3.PublicKey, password: 
 
 describe('\n\n\n============== CREATE REPUTATION ACCOUNT  =================\n\n', () => {	
   it('Initializes User reputation account !', async () => {
-    const secret = "password" ;
     //create payerd
-    const {tx , reputation} = await createRepAccount(secret);
+    const {tx } = await createRepAccount();
     console.log(`Created the user reputation account transaction link: ${tx}`);
     const rep  = await program.account.reputation.fetch(reputation);
     console.log(`\nSources count is : ${rep.sourcesCount.toString()}`);
@@ -65,20 +93,18 @@ describe('\n\n\n============== CREATE REPUTATION ACCOUNT  =================\n\n'
 
 describe('\n\n\n============= CREATE A SOURCE ACCOUNT =====================\n\n', () => {
   it("creates source account", async ()=> {
-    const secret = "addsources" ;
     const sourceName = "dummy" ;
     //create reputation account to use to create source account 
-    const {tx , reputation} = await createRepAccount(secret);
     const rep  = await program.account.reputation.fetch(reputation);
     assert.equal(rep.sourcesCount.toNumber(), 0 , 'source count should be zero') ;
     assert.equal(rep.attachedAccount.toString(), payer.publicKey.toString(), "publick key of attached account not same as payer");
 
-    const {source, tx2 } = await createSourceAccount(reputation, secret ,sourceName ) ;
+    const {source, tx2 } = await createSourceAccount(reputation ,sourceName ) ;
     console.log(`Created the user source account transaction link: ${tx2}`);
     console.log(`\n\nThe source account is:${source}\n`)
 
     const reloadRep  = await program.account.reputation.fetch(reputation);
-    console.log(`\n The user source count is now:${reloadRep.sourcesCount.toNumber()}\n`)
+    console.log(`\nThe user source count is now:${reloadRep.sourcesCount.toNumber()}\n`)
     assert.equal(reloadRep.sourcesCount.toNumber(), 1 , 'source count should be one') ;
     assert.equal(reloadRep.attachedAccount.toString(), payer.publicKey.toString(), "publick key of attached account not same as payer");
 
@@ -88,10 +114,49 @@ describe('\n\n\n============= CREATE A SOURCE ACCOUNT =====================\n\n'
     console.log(`\nSource points: ${sc.points}\n`);
     assert.equal(sc.name , sourceName , `source name should be ${sourceName}`) ;
     assert.equal(sc.points.toNumber(), 0 , "source points should be 0");
-
-
   })
 
 })
 
 
+describe('\n\n\n============= UPDATE USER SOURCE ACCOUNT ==================\n\n', () => {
+  it("Adds and subtracts points from source accounts", async ()=> {
+    const sourceName = "dummy2" ;
+    //create reputation account to use to create source account 
+    const rep  = await program.account.reputation.fetch(reputation);
+    assert.equal(rep.sourcesCount.toNumber(), 1 , 'source count should be zero') ;
+    assert.equal(rep.attachedAccount.toString(), payer.publicKey.toString(), "publick key of attached account not same as payer");
+
+    //create source account
+    const {source, tx2 } = await createSourceAccount(reputation ,sourceName ) ;
+
+    const reloadRep  = await program.account.reputation.fetch(reputation);
+    assert.equal(reloadRep.sourcesCount.toNumber(), 2 , 'source count should be one') ;
+    assert.equal(reloadRep.attachedAccount.toString(), payer.publicKey.toString(), "publick key of attached account not same as payer");
+
+    // source. 
+    const sc = await program.account.source.fetch(source);
+    assert.equal(sc.name , sourceName , `source name should be ${sourceName}`) ;
+    assert.equal(sc.points.toNumber(), 0 , "source points should be 0");
+
+    const bonus = 3 ;
+    const {tx3} = await addPoints(bonus, source, sourceName) ;
+    const sc2 = await program.account.source.fetch(source);
+    console.log(`Added reputation transaction link: ${tx3}`);
+    console.log(`\nSource points now equal : ${sc2.points.toString()}\n`);
+    assert.equal(sc2.name , sourceName , `source name should be ${sourceName}`) ;
+    assert.equal(sc2.points.toNumber(), 3 , "source points should be 3");
+
+    const penalty = 1 ;
+
+    const {tx4} = await subtractPoints(penalty,source, sourceName) ;
+    const sc3 = await program.account.source.fetch(source);
+    console.log(`\nSource points now equal : ${sc3.points.toString()}\n`);
+    console.log(`Removed reputation transaction link: ${tx4}`);
+    assert.equal(sc3.name , sourceName , `source name should be ${sourceName}`) ;
+    assert.equal(sc3.points.toNumber(), 2 , "source points should be 2");
+
+  })
+
+
+})
