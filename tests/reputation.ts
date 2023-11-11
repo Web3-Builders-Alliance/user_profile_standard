1a98349c126c8ecbd847874f287fbf5ba79d1d87
victor_tk <victorkdevelopment@gmail.com>
2023-11-11 17:31:03 +0200

    create source  acount testi setup cont

--- tests/reputation.ts
+++ tests/reputation.ts
@@ -12,48 +12,47 @@ const payer = provider.wallet as anchor.Wallet;
 // const secret = "password" 
 
 const createRepAccount = async (secret) => {
-    // Create reputation account
-    const [reputation] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from('reputation'),Buffer.from(secret)],
-      program.programId
-    );
-    const tx = await program.methods
-      .createReputationAccount(
-        secret
-      )
-      .accounts({
-        reputation,
-        payer: payer.payer.publicKey
-      })
-      .rpc();
+  // Create reputation account
+  const [reputation] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from('reputation'),Buffer.from(secret)],
+    program.programId
+  );
+  const tx = await program.methods
+    .createReputationAccount(
+      secret
+    )
+    .accounts({
+      reputation,
+      payer: payer.payer.publicKey
+    })
+    .rpc();
   return { reputation, tx } 
 }
 
 const createSourceAccount = async (reputation: anchor.web3.Keypair ) => {
-    // Create source account
-    const [dummy_source] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from('source_name'),reputation.publicKey.toBuffer()],
-      program.programId
-    );
-    const tx = await program.methods
-      .createSourceAccount(
-      )
-      .accounts({
-        reputation,
-        dummy_source,
-        payer: payer.payer.publicKey,
-        
-      })
-      .rpc();
+  // Create source account
+  const [dummy_source] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from('source_name'),reputation.publicKey.toBuffer()],
+    program.programId
+  );
+  const tx = await program.methods
+    .createSourceAccount(
+    )
+    .accounts({
+      reputation,
+      dummy_source,
+      payer: payer.payer.publicKey,
+
+    })
+    .rpc();
   return { reputation, tx } 
 }
 
-describe('Create reputation account', () => {	
+describe('\n\n\n============== CREATE REPUTATION ACCOUNT  =================\n\n', () => {	
   it('Initializes User reputation account !', async () => {
     const secret = "password" ;
     //create payerd
     const {tx , reputation} = await createRepAccount(secret);
-    console.log('\n\n\n ============== CREATE REPUTATION ACCOUNT  =================\n\n');
     console.log(`Created the user reputation account transaction link: ${tx}`);
     const rep  = await program.account.reputation.fetch(reputation);
     console.log(`\nSources count is : ${rep.sourcesCount.toString()}`);
@@ -63,9 +62,8 @@ describe('Create reputation account', () => {
   });
 })
 
-describe('Add sources to a user reputation account', () => {
+describe('\n\n\n============= CREATE A SOURCE ACCOUNT =====================\n\n', () => {
   it("creates source account", async ()=> {
-    console.log("\n\n\n============= CREATE A SOURCE ACCOUNT =====================\n\n");
     const secret = "addsources" ;
     //create reputation account to use to create source account 
     const {tx , reputation} = await createRepAccount(secret);
@@ -75,17 +73,17 @@ describe('Add sources to a user reputation account', () => {
 
     const {source, tx2 } = await createSourceAccount() ;
     console.log(`Created the user source account transaction link: ${tx}`);
-    console.log(`\n\n The source account is:${source}\n`)
-    
+    console.log(`\n\nThe source account is:${source}\n`)
+
     const reloadRep  = await program.account.reputation.fetch(reputation);
     console.log(`\n The user source count is now:${reloadRep.sourcesCount.toNumber()}\n`)
     assert.equal(reloadRep.sourcesCount.toNumber(), 1 , 'source count should be one') ;
     assert.equal(reloadRep.attachedAccount.toString(), payer.publicKey.toString(), "publick key of attached account not same as payer");
-    
+
     // source. 
 
   })
-    
+
 })
 
 
