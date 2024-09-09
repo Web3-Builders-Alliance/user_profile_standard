d9ef8485b72e22384b3407b54789a31e3816994f
victor_tk <victorkdevelopment@gmail.com>
2024-09-09 13:26:02 +0200

    init reputation ui

---
+++ frontend/reputation/app/page.tsx
@@ -0,0 +1,24 @@
+import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from 'next/link';
+
+export default function Home() {
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.create}>
+        <Typography variant="h1" gutterBottom> 
+          Create Your Reputation account On solana here!
+        </Typography>
+        <Link href="/create">
+        <Button className="create-button" size="large" variant="contained">
+          <Typography variant="h3">
+            Create Reputation
+          </Typography>
+          </Button>
+        </Link>
+      </main>
+    </div>
+  );
+}
