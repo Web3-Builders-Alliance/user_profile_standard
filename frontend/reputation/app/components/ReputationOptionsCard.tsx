3c1472c223140f5c4a9e270fd971bc7b6b7aa122
victor_tk <victorkdevelopment@gmail.com>
2024-09-26 22:58:45 +0200

    reputation options

--- frontend/reputation/app/components/ReputationOptionsCard.tsx
+++ frontend/reputation/app/components/ReputationOptionsCard.tsx
@@ -6,95 +6,111 @@ import Button from '@mui/material/Button';
 import RepData from "./RepData"
 import styles from './styles/reputationOptionsCard.module.css'
 
+
+const ReputationOptionsCard = (props) => {
+  const [signIn, setSignIn ] = useState(false);
+  const [create, setCreate ] = useState(false);
+  const [loadRep, setLoadRep] = useState(false);
+  const [userId, setUserId] = useState("");
+
+
+const Reputation = () => {
+  return(
+    <RepData authority={userId}/>
+  ) 
+}
+
 const SignIn = (props) => {
   return( 
-        <Card variant="outlined" className={styles.wrapper}>
-          <Box><Typography>Sign In</Typography></Box>        
-          <RepData/>
-          <Box
-            component="form" 
-            noValidate
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              gap: 2,
-            }}
-          >
-            <TextField id="standard-basic" label="authority" variant="standard" />         
-            <Button  onClick={ (e) => props.handleDelete(e)}>Find</Button>
-            <Button  onClick={ (e) =>props.handleCreate(e)}>Create</Button>
-          </Box>
-        </Card>
+    <Card variant="outlined" className={styles.wrapper}>
+      <Box className={styles.box}>
+        <Box className={styles.createText}>
+          <Typography variant="h6" sx={{color: "#8596B1"}} >Already Have</Typography>
+          <Typography  variant="h5" sx={{color: "#72A2EE"}} >Reputation Account</Typography> 
+          <Typography sx={{color: "#8596B1"}} variant="body1" >Sign In</Typography>
+        </Box>        
+        <Form  handleCreate={props.handleCreate} handleSignIn={props.handleSignIn} />
+      </Box>
+    </Card>
   ) 
 }
 
 const Create  = (props) => {
   return( 
-        <Card variant="outlined" className={styles.wrapper}>
-          <Box><Typography>Create Reputation Account</Typography></Box>        
-          <RepData/>
-          <Box
-            component="form" 
-            noValidate
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              gap: 2,
-            }}
-          >
-            <TextField id="standard-basic" label="authority" variant="standard" />         
-            <Button  onClick={ (e) => props.handleDelete(e)}>Find</Button>
-            <Button  onClick={ (e) =>props.handleCreate(e)}>Create</Button>
-          </Box>
-        </Card>
+    <Card variant="outlined" className={styles.wrapper}>
+      <Box className={styles.box}>
+        <Box className={styles.createText}>
+          <Typography variant="h6" sx={{color: "#8596B1"}} >Create</Typography>
+          <Typography  variant="h5" sx={{color: "#72A2EE"}} >Reputation Account</Typography> 
+          <Typography sx={{color: "#8596B1"}} variant="body1" >Join The Community</Typography>
+        </Box>        
+        <Form  handleCreate={props.handleCreate} handleSignIn={props.handleSignIn} />
+      </Box>
+    </Card>
   ) 
 }
 
 const First = (props) => {
   return( 
-        <Card variant="outlined" className={styles.wrapper}>
-          <Box className={styles.innerWrapper}>
-          <Box className={styles.content}>
+    <Card variant="outlined" className={styles.wrapper}>
+      <Box className={styles.innerWrapper}>
+        <Box className={styles.content}>
           <Box className={styles.text}>
-          <Typography sx={{color: "#8596B1"}} variant="h6">Your
-            <Typography sx={{color: "#72A2EE"}} variant="h4">Reputation</Typography>
+            <Typography sx={{color: "#8596B1"}} variant="h6">Your
+              <Typography sx={{color: "#72A2EE"}} variant="h4">Reputation</Typography>
             </Typography>
-          <Typography sx={{color: "#8596B1"}} variant="h6">Account is the Central Source for all your</Typography>
-          <Typography sx={{color: "#8596B1"}} variant="h6">Reputation Identity Information</Typography>
+            <Typography sx={{color: "#8596B1"}} variant="h6">Account is the Central Source for all your</Typography>
+            <Typography sx={{color: "#8596B1"}} variant="h6">Reputation Identity Information</Typography>
           </Box>        
           <Box
             className={styles.buttons}
           >
-            <Button size="large" onClick={ (e) => props.handleInit(e)}>Sign In</Button>
+            <Button size="large" onClick={ (e) => props.handleSignIn(e)}>Sign In</Button>
             <Button size="large" onClick={ (e) =>props.handleCreate(e)}>Sign Up</Button>
           </Box>
-          </Box>
-          </Box>
-        </Card>
+        </Box>
+      </Box>
+    </Card>
   ) 
 }
 
-const ReputationOptionsCard = (props) => {
-  const [signIn, setSignIn ] = useState(false);
-  const [create, setCreate ] = useState(false);
+const Form = (props) => {
+  return( <Box className={styles.repForm}>
+    <Box component="form" className={styles.textField}>
+      <Typography variant="body1" sx={{color: "#DECEC9"}}>Enter Authority ID</Typography> 
+      <TextField value={userId} onChange= {(e) => setUserId(e.target.value)} id="standard-basic" label="authority" variant="standard" />         
+    </Box>
+    <Box className={styles.createButtons}>
+      <Button size='small' onClick={ (e) => props.handleSignIn(e)}>Sign In</Button>
+      <Button size='small' onClick={ (e) =>props.handleCreate(e)}>Create</Button>
+    </Box>
+  </Box>
+  )
+}
 
-const handleInitButton = (e) =>{ 
+  const handleSignInButton = (e) =>{ 
     setCreate(false)
     setSignIn(true)
-}
-const handleCreateButton = (e) => {
+  }
+  const handleCreateButton = (e) => {
     setSignIn(false)
     setCreate(true)
-}
+  }
+
+  const searchRep = (e) => {
+    e.preventDefault();
+    alert(` the what what ${userId}`)
+    setSignIn(false)
+    setLoadRep(true) 
+  }
 
   return( 
-    signIn? <SignIn/>:
-    create? <Create/>:
-    <First handleCreate= {handleCreateButton} handleInit={handleInitButton} />
+    signIn? <SignIn  handleCreate= {handleCreateButton} handleSignIn={searchRep} />:
+    create? <Create handleCreate={props.handleCreate} handleSignIn={handleSignInButton}/>:
+    loadRep? <Reputation/>:
+        <First handleCreate= {handleCreateButton} handleSignIn={handleSignInButton} />
   )
-  
+
 }
 
 export default ReputationOptionsCard
